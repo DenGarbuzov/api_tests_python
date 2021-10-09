003a46c9a92c5333a749ac91645f921c22efd6a8
den <dengarbuzov@mail.ru>
2021-10-10 02:55:03 +0300

    tests added

--- test_api.py
+++ test_api.py
@@ -1,24 +1,158 @@
+import enum
+from datetime import datetime
+from enum import Enum
+from time import sleep
+
+import allure
+import pytest
 import requests as requests
+from allure_commons.types import AttachmentType
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
-from selenium.webdriver.support.wait import WebDriverWait
+from pathlib import Path
+
+# путь до локального драйвера Firefox в проекте
+
+path_to_geckodriver = Path("geckodriver").resolve()
+execute_firefox = webdriver.Firefox(executable_path=path_to_geckodriver)
+
+main_url = "https://mail.ru"  # основная ссылка для теста
+
+
+# Allure классификация
+@enum.unique
+class Feature(Enum):
+    CLICK_ELEMENT = 'Нажатие на элемент'
+    CHECK_EXIST_ELEMENT = 'Проверка наличия элемента'
+    FUNC_TEST = 'Функциональное тестирование'
+
+
+# Allure классификация
+@enum.unique
+class Level(Enum):
+    """
+    По убыванию - в зависимости от важности
+    """
+    BLOCKER = 'blocker'
+    CRITICAL = 'critical'
+    NORMAL = 'normal'
+    MINER = 'miner'
+    TRIVIAL = 'trivial'
 
 
 class TestSomeThing:
 
-    def test_get_200(self):
-        response = requests.get("https://habr.com/ru/all")
+    @allure.feature(Feature.CHECK_EXIST_ELEMENT.value)
+    @allure.story('Проверка на работоспособность сайта,'
+                  'в случае удачи поиск всех доступных ссылок,'
+                  'вывод в файлы отчета об ответе')
+    @allure.severity(severity_level=Level.CRITICAL.value)
+    def test_get_all_href_respond(self):
 
-        print(response.status_code)
-        PATH = "Users/den/Desktop/api_tests/geckodriver"
-        with webdriver.Firefox(PATH) as driver:
+        # очистка файлов отчета для новой записи
 
-            wait = WebDriverWait(driver, 10)
-            driver.get("https://habr.com/ru/all/")
-            list_of_all_links = driver.find_elements_by_name("href").sort(str.startswith("https://"))
-            print(list_of_all_links)
-            # first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3")))
-            # print(first_result.get_attribute("textContent"))
+        clear_issue_report = open('response_issues.txt', 'w')
+        clear_issue_report.write("Test started at " + str(datetime.today()) + "\n" + "\n")
+        clear_issue_report.close()
 
+        clear_positive_report = open('response_200.txt', 'w')
+        clear_positive_report.write("Test started at " + str(datetime.today()) + "\n" + "\n")
+        clear_positive_report.close()
+
+        response = requests.get(main_url)
+        answer = response.status_code
+
+        # Проверка доступности основной ссылки
+
+        if 200 <= answer < 300:
+            with execute_firefox as driver:  # запуск драйвера на полный экран
+
+                driver.maximize_window()
+                driver.get(main_url)
+
+                # скриншот открытой страницы для Allure отчета
+
+                allure.attach(driver.get_screenshot_as_png(), name="output result",
+                              attachment_type=AttachmentType.PNG)
+                links = driver.find_elements_by_tag_name("a")
+
+                # получение списка элементов содержащих ссылки
+
+                for lnk in links:
+
+                    # get_attribute() получение всех атрибутов ссылок в дереве элементов
+
+                    testing_link = lnk.get_attribute('href')
+
+                    # условие что ссылка не пустая строка
+
+                    if testing_link is not None:
+
+                        if testing_link.startswith("/"):
+                            testing_link = main_url + testing_link
+                            # отправка запроса в полученную с сайта ссылку
+
+                            response = requests.get(testing_link)
+                            answer = response.status_code
+                        else:
+                            pass
+
+                        if answer == 200:  # условие ответа равного 200
+
+                            # запись информации в файл
+
+                            positive_report_file = open("response_200.txt", "a+")
+                            positive_report_file.write("\n" + " link status is " + str(answer) +
+                                                       " " + str(datetime.now()) + "\n" + testing_link + "\n")
+                            positive_report_file.close()
+
+                        elif answer != 200:  # условие ответа не равного 200
+
+                            # запись информации в файл
+
+                            issue_report_file = open("response_issues.txt", "a+")
+                            issue_report_file.write("\n" + " link status is " + str(answer) +
+                                                    " " + str(datetime.now()) + "\n" + testing_link + "\n")
+                            issue_report_file.close()
+
+                        else:
+                            continue
+        else:
+            pytest.fail('Site is dead. Link is broken')
+
+    @allure.feature(Feature.FUNC_TEST.value)
+    @allure.story('Переход по всем ссылкам сайта и сверка адреса с ссылкой оригинала')
+    @allure.severity(severity_level=Level.CRITICAL.value)
+    def test_all_href_correction_transition(self):
+
+        # инициализация драйвера
+
+        with execute_firefox as driver:
+
+            driver.maximize_window()
+            driver.implicitly_wait(5)
+
+            # открытие искомой страницы
+
+            driver.get(main_url)
+            allure.attach(driver.get_screenshot_as_png(), name="output result",
+                          attachment_type=AttachmentType.PNG)
+
+            # скриншот искомой страницы
+
+            links = driver.find_elements_by_tag_name("a")
+            for lnk in links:
+                testing_link = lnk.get_attribute('href')
+
+                if testing_link is not None:  # условие что ссылка не пустая строка
+
+                    if testing_link.startswith("/"):
+                        # условие если ссылка не полная
+                        testing_link = main_url + testing_link
+
+                    another_driver = execute_firefox
+                    another_driver.execute_script("window.open('');")  # открытие дополнительного окна браузера
+                    another_driver.switch_to.window(driver.window_handles[1])  # переход в окно браузера
+                    another_driver.get(testing_link)  # открытие ссылки
+                    sleep(3)
+                    assert testing_link == another_driver.current_url  # проверка на соответствие текущей ссылке
+                    another_driver.switch_to.window(driver.window_handles[0])  # возврат к основному окну
